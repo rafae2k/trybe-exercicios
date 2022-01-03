78196e001a7728ba8d8fbc4aecfa7de8603f72e0
rafae2k <53544360+rafae2k@users.noreply.github.com>
2022-01-03 16:59:24 -0300

    exercise 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -111,4 +111,14 @@ const moreThan60y = books
   })
   .sort((a, b) => a.releaseYear - b.releaseYear);
 
-console.log(moreThan60y);
+// console.log(moreThan60y);
+
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+const fantasyOrScienceFictionAuthors = filterByGenre
+  .map((book) => {
+    return book.author.name;
+  })
+  .sort();
+
+// console.log(fantasyOrScienceFictionAuthors);
