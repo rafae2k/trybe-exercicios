cf4b00e9726e1a8890af0f9d9c5b0bc43557f0a5
rafae2k <53544360+rafae2k@users.noreply.github.com>
2022-01-03 17:03:37 -0300

    exercise 6

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -122,3 +122,9 @@ const fantasyOrScienceFictionAuthors = filterByGenre
   .sort();
 
 // console.log(fantasyOrScienceFictionAuthors);
+
+// 6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+
+const oldBooks = moreThan60y.map((book) => book.name);
+
+// console.log(oldBooks);
